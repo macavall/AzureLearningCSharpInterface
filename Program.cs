f6412936ed6fe2eb026d892d3667c82be6619b5c
Matthew Cavallo <macavall@microsoft.com>
2024-02-28 12:15:36 -0600

    First

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Emit;
+
+using System;
+
+namespace proj1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        var myClass = new MyClass();
+
+        //myClass.MyClassMethod();
+        myClass.MyClassProperty = "Hello!";
+    }
+
+    public interface IMyClass
+    {
+        public string? MyClassProperty {get; set;}
+        public void MyClassMethod();
+        public string MyOtherMethod();
+        
+    }
+
+    public class MyClass : IMyClass
+    {
+        private string? myClassField;
+
+        public string? MyClassProperty 
+        {
+            get
+            {
+                return myClassField;
+            }
+
+            set
+            {
+                this.MyClassMethod();
+                myClassField = value ?? "NULL";
+            }
+        }
+
+        public void MyClassMethod()
+        {
+            Type[] interfaces = this.GetType().GetInterfaces();
+
+            foreach (Type item in interfaces)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+
+            System.Console.WriteLine("MyClassMethod!");
+        }
+    }
+}
